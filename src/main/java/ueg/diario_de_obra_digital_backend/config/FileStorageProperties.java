83161684528668f260a47cac8e6ee01b683bc51b
JP <jplmarotinho22@gmail.com>
2025-10-01 16:42:45 -0300

    FileStorageProperties: correção adição de @Configuration

--- src/main/java/ueg/diario_de_obra_digital_backend/config/FileStorageProperties.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/config/FileStorageProperties.java
@@ -3,11 +3,13 @@ package ueg.diario_de_obra_digital_backend.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-
+@Configuration
 @ConfigurationProperties(prefix = "file") //prefixo no application.properties
+
 public class FileStorageProperties {
   private String uploadDir;
 }
