24ea2a0bacd30603b3642251b5d0c22a22516e18
JP <jplmarotinho22@gmail.com>
2025-10-01 14:16:16 -0300

    FileStorageService: métodos iniciais storeFile e loadFileAsResource

--- src/main/java/ueg/diario_de_obra_digital_backend/service/FileStorageService.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/service/FileStorageService.java
@@ -1,12 +1,24 @@
 package ueg.diario_de_obra_digital_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 import ueg.diario_de_obra_digital_backend.config.FileStorageProperties;
+import ueg.diario_de_obra_digital_backend.service.exception.FileNotFoundInStorageException;
+import ueg.diario_de_obra_digital_backend.service.exception.FileStorageCreationException;
+import ueg.diario_de_obra_digital_backend.service.exception.InvalidFileName;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 @Service
 public class FileStorageService {
@@ -26,6 +38,48 @@ public class FileStorageService {
       Files.createDirectories(this.fileStorageLocation);
     } catch (Exception e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  public String storeFile(MultipartFile file) {
+    String originalFileName = StringUtils
+        .cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+
+    if(originalFileName.contains("..")) {
+      throw new InvalidFileName("Nome do arquivo inválido, renomeie e tente novamente.");
+    }
+
+    System.out.print(originalFileName);
+
+    // efetivamente salva o arquivo no diretório
+    try {
+      // constrói o caminho de destino, usando o método resolve
+      Path targetLocation = this.fileStorageLocation.resolve(originalFileName);
+      try (InputStream inputStream = file.getInputStream()) {
+        Files.copy(inputStream, targetLocation, StandardCopyOption.REPLACE_EXISTING);
+      }
+
+      // posteriormente o nome será tratado, por isso o retorno vai ser um nome que contém data e outras informações relevantes
+      return originalFileName;
+    } catch (IOException e) {
+      throw new FileStorageCreationException("Não foi possível salvar o arquivo");
+    }
+  }
+
+  // método que carrega um arquivo do storage como um Resource
+  public Resource loadFileAsResource(String fileName) {
+    try {
+      // tenta encontrar o arquivo dentro do storage
+      Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+      Resource resource = new UrlResource(filePath.toUri()); //UrlResource pode lançar uma MalformedURLException
+
+      if(resource.exists()) {
+        return resource;
+      } else {
+        throw new FileNotFoundInStorageException("Arquivo não encontrado no storage: " + fileName);
+      }
+    } catch (MalformedURLException e) {
+      throw new FileNotFoundInStorageException("Arquivo não encontrado no storage: " + fileName);
     }
   }
 }
