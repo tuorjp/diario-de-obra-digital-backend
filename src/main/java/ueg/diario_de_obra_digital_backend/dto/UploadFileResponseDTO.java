7652380ae51b34bdf88d9b11f0690ed7f711ff66
JP <jplmarotinho22@gmail.com>
2025-10-01 14:00:55 -0300

    UploadFileResponseDTO

---
+++ src/main/java/ueg/diario_de_obra_digital_backend/dto/UploadFileResponseDTO.java
@@ -0,0 +1,11 @@
+package ueg.diario_de_obra_digital_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class UploadFileResponseDTO {
+  private String fileName;
+  private String dileDownloadUri;
+  private String fileType;
+  private long size;
+}
