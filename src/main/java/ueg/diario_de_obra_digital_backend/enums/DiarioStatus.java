a2d4bd424143571750c8d1019da4b070cd108dce
JP <jplmarotinho22@gmail.com>
2025-10-01 15:03:13 -0300

    Outros Enums criados

---
+++ src/main/java/ueg/diario_de_obra_digital_backend/enums/DiarioStatus.java
@@ -0,0 +1,7 @@
+package ueg.diario_de_obra_digital_backend.enums;
+
+public enum DiarioStatus {
+  AGUARDANDO_AVALIACAO,
+  VALIDO,
+  INVALIDO
+}
---
+++ src/main/java/ueg/diario_de_obra_digital_backend/enums/ObraStatus.java
@@ -0,0 +1,6 @@
+package ueg.diario_de_obra_digital_backend.enums;
+
+public enum ObraStatus {
+  ATIVA,
+  INATIVA
+}
