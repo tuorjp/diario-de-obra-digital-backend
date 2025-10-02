c993de0b1207eebd159e7b1fa138c1303b2b4ffa
JP <jplmarotinho22@gmail.com>
2025-10-02 16:45:05 -0300

    DiarioDeObraRepository: método de listagem incluindo deletados criado.

--- src/main/java/ueg/diario_de_obra_digital_backend/repository/DiarioDeObraRepository.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/repository/DiarioDeObraRepository.java
@@ -1,6 +1,8 @@
 package ueg.diario_de_obra_digital_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ueg.diario_de_obra_digital_backend.model.DiarioDeObra;
 import ueg.diario_de_obra_digital_backend.model.Obra;
@@ -11,4 +13,6 @@ import java.util.Optional;
 @Repository
 public interface DiarioDeObraRepository extends JpaRepository<DiarioDeObra, Long> {
   Optional<DiarioDeObra> findByObraAndData(Obra obra, LocalDate data);
+  @Query(value = "SELECT * FROM diarios_de_obra WHERE id = :id", nativeQuery = true)
+  Optional<DiarioDeObra> findByIdEvenIfDeleted(@Param("id") Long id);
 }
