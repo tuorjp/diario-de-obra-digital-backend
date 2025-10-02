94d619d525ceea40671f8d0c979261a0ceef86a9
JP <jplmarotinho22@gmail.com>
2025-10-02 15:03:18 -0300

    AuthenticationController, AuthorizationService: função authenticate e limpeza do código

--- src/main/java/ueg/diario_de_obra_digital_backend/controller/AuthenticationController.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/controller/AuthenticationController.java
@@ -1,10 +1,8 @@
 package ueg.diario_de_obra_digital_backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,43 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 import ueg.diario_de_obra_digital_backend.config.security.TokenService;
 import ueg.diario_de_obra_digital_backend.dto.AuthenticationDTO;
 import ueg.diario_de_obra_digital_backend.dto.LoginResponseDTO;
-import ueg.diario_de_obra_digital_backend.dto.RegisterDTO;
-import ueg.diario_de_obra_digital_backend.model.User;
-import ueg.diario_de_obra_digital_backend.repository.UserRepository;
+import ueg.diario_de_obra_digital_backend.service.AuthorizationService;
 
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-  private final AuthenticationManager authenticationManager;
-  private final UserRepository userRepository;
   private final TokenService tokenService;
-  private final PasswordEncoder passwordEncoder;
+  private final AuthorizationService authorizationService;
 
   @PostMapping("login")
-  public ResponseEntity login(@RequestBody AuthenticationDTO data) {
-    var usernamePassword = new UsernamePasswordAuthenticationToken(data.getLogin(), data.getPassword());
-
-    var auth = this.authenticationManager.authenticate(usernamePassword);
-
-    var token = tokenService.generateToken((User) auth.getPrincipal());
+  public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data) {
+    var token = authorizationService.authenticate(data);
 
     LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
     loginResponseDTO.setToken(token);
 
-    return ResponseEntity.ok(loginResponseDTO);
-  }
-
-  @PostMapping("register")
-  public ResponseEntity register(@RequestBody RegisterDTO data) {
-    if(this.userRepository.findByLogin(data.getLogin()) != null) {
-      return ResponseEntity.badRequest().build();
-    }
-
-    String encryptedPassword = passwordEncoder.encode(data.getPassword());
-    User newUser = new User(data.getLogin(), data.getName(), encryptedPassword, data.getRole());
-
-    userRepository.save(newUser);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.status(HttpStatus.OK).body(loginResponseDTO);
   }
 }
--- src/main/java/ueg/diario_de_obra_digital_backend/service/AuthorizationService.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/service/AuthorizationService.java
@@ -1,10 +1,15 @@
 package ueg.diario_de_obra_digital_backend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ueg.diario_de_obra_digital_backend.config.security.TokenService;
+import ueg.diario_de_obra_digital_backend.dto.AuthenticationDTO;
+import ueg.diario_de_obra_digital_backend.model.User;
 import ueg.diario_de_obra_digital_backend.repository.UserRepository;
 
 // Esse service precisa implementar o UserDetailsService, para usar o método de encontrar o usuário por login (no nosso caso o email)
@@ -12,9 +17,21 @@ import ueg.diario_de_obra_digital_backend.repository.UserRepository;
 @RequiredArgsConstructor
 public class AuthorizationService implements UserDetailsService {
   private final UserRepository userRepository;
+  private final AuthenticationManager authenticationManager;
+  private final TokenService tokenService;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     return userRepository.findByLogin(username); //no nosso caso vai ser o email
+  }
+
+  public String authenticate(AuthenticationDTO data) {
+    var usernamePassword = new UsernamePasswordAuthenticationToken(data.getLogin(), data.getPassword());
+
+    var auth = this.authenticationManager.authenticate(usernamePassword);
+
+    var token = tokenService.generateToken((User) auth.getPrincipal());
+
+    return token;
   }
 }
