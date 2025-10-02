e7ed16443529a7290111db37a4d58946b2275dbb
JP <jplmarotinho22@gmail.com>
2025-10-02 15:23:54 -0300

    AuthenticationController e AuthorizationService: corrigindo erro de referência circular

--- src/main/java/ueg/diario_de_obra_digital_backend/controller/AuthenticationController.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/controller/AuthenticationController.java
@@ -3,6 +3,8 @@ package ueg.diario_de_obra_digital_backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 import ueg.diario_de_obra_digital_backend.config.security.TokenService;
 import ueg.diario_de_obra_digital_backend.dto.AuthenticationDTO;
 import ueg.diario_de_obra_digital_backend.dto.LoginResponseDTO;
-import ueg.diario_de_obra_digital_backend.service.AuthorizationService;
+import ueg.diario_de_obra_digital_backend.model.User;
 
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
   private final TokenService tokenService;
-  private final AuthorizationService authorizationService;
+  private final AuthenticationManager authenticationManager;
 
   @PostMapping("login")
   public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data) {
-    var token = authorizationService.authenticate(data);
+    // Cria um token de autenticação com as credenciais recebidas
+    var usernamePassword = new UsernamePasswordAuthenticationToken(data.getLogin(), data.getPassword());
 
+    // O AuthenticationManager usa o seu AuthorizationService por baixo dos panos para
+    // buscar o usuário e verificar a senha.
+    var auth = this.authenticationManager.authenticate(usernamePassword);
+
+    // Se a autenticação foi bem-sucedida, gera o token JWT
+    var token = tokenService.generateToken((User) auth.getPrincipal());
+
+    // Retorna o token em um DTO
     LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
     loginResponseDTO.setToken(token);
 
--- src/main/java/ueg/diario_de_obra_digital_backend/service/AuthorizationService.java
+++ src/main/java/ueg/diario_de_obra_digital_backend/service/AuthorizationService.java
@@ -1,37 +1,22 @@
 package ueg.diario_de_obra_digital_backend.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ueg.diario_de_obra_digital_backend.config.security.TokenService;
-import ueg.diario_de_obra_digital_backend.dto.AuthenticationDTO;
-import ueg.diario_de_obra_digital_backend.model.User;
 import ueg.diario_de_obra_digital_backend.repository.UserRepository;
 
 // Esse service precisa implementar o UserDetailsService, para usar o método de encontrar o usuário por login (no nosso caso o email)
+// A única responsabilidade desse service é a função que está criada dentro dele
+// !! NÃO MEXER !!
 @Service
 @RequiredArgsConstructor
 public class AuthorizationService implements UserDetailsService {
   private final UserRepository userRepository;
-  private final AuthenticationManager authenticationManager;
-  private final TokenService tokenService;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     return userRepository.findByLogin(username); //no nosso caso vai ser o email
-  }
-
-  public String authenticate(AuthenticationDTO data) {
-    var usernamePassword = new UsernamePasswordAuthenticationToken(data.getLogin(), data.getPassword());
-
-    var auth = this.authenticationManager.authenticate(usernamePassword);
-
-    var token = tokenService.generateToken((User) auth.getPrincipal());
-
-    return token;
   }
 }
